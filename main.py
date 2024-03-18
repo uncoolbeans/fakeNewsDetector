e53e21d0a3d8e041e2bd6880e39d5ed6c4576c1e
Bill Nguyen <ivecleandedmyroom@gmail.com>
2024-03-18 20:42:54 +1100

    added simple  text preprocessing

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 import pandas as pd
 import numpy as np
+import re
 
 true = pd.read_csv('True.csv')
 fake = pd.read_csv('Fake.csv')
 
-print(true.head())
+true['label'] = 1
+fake['label'] = 0
+
+data = pd.concat([fake,true],axis = 0)
+data = data.drop(['subject','date'],axis = 1)
+
+data = data.sample(frac=1) #shuffles the dataset
+data.reset_index(inplace=True)
+data.drop(['index'], axis = 1, inplace = True)
+
+
+def preProcess(text): #make the text suitable for the machine to read, eliminate irrelevant bits
+
+    #lowercase everything
+    text = text.lower()
+    #removing html embeds
+    text = re.sub(r'https?://\S+\www\.\S+','',text)
+    #remove punctuation
+    text = re.sub(r'<.*?>','', text)
+    #remove digits
+    text = re.sub(r'\d','',text)
+    #remove newlines
+    text = re.sub(r'\n',' ', text)
+
+    return text
+    
+data['text'] = data['text'].apply(preProcess)
+
+print(data['text'])
+
+
