548faaa67f4dc8d59b59b67fc41f812caf65ff05
Bill Nguyen <ivecleandedmyroom@gmail.com>
2024-03-20 22:18:44 +1100

    aaaaaaaaaaaa

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import pandas as pd
 import numpy as np
 import re
+import nltk
+from sklearn.model_selection import train_test_split
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+import string
+
+
+
+#nltk.download('stopwords')
+#nltk.download('punkt')
+#nltk.download('wordnet')
+#nltk.download('omw-1.4')
+#
+#stopWords = set(stopwords.words('english')) 
+#print(stopWords)
+#
+#stemmer = PorterStemmer()
+#lemmatizer = WordNetLemmatizer()
 
 true = pd.read_csv('True.csv')
 fake = pd.read_csv('Fake.csv')
@@ -29,10 +48,25 @@ def preProcess(text): #make the text suitable for the machine to read, eliminate
     #remove newlines
     text = re.sub(r'\n',' ', text)
 
+    #words = word_tokenize(text)
+#
+    ##removing stopwords
+    #words = [word for word in words if word not in stopWords]
+#
+    ##lemmatizing text
+    #words = [stemmer.stem(word) for word in words]
+#
+    #text = ''.join(words)
+#
     return text
+
+print('preprocessing.....')
     
 data['text'] = data['text'].apply(preProcess)
 
 print(data['text'])
+
+x = data['text']
+y = data['label']
 
 
